d6b8c7721300b7a97f6eb0ca585915165600cbb8
Max <nginanie@gmail.com>
2026-05-03 17:21:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,34 @@ function submitQuiz() {
         "2. Inheritance = One class uses another<br>" +
         "3. Polymorphism = One method, many forms";
 }
+
+function sendMessage() {
+    let input = document.getElementById("userInput").value.toLowerCase();
+    let chat = document.getElementById("chatBody");
+
+    let response = "Sorry, I don't understand.";
+
+    if (input.includes("oop")) {
+        response = "OOP stands for Object-Oriented Programming.";
+    } 
+    else if (input.includes("encapsulation")) {
+        response = "Encapsulation means wrapping data and methods together.";
+    }
+    else if (input.includes("inheritance")) {
+        response = "Inheritance allows one class to use another class.";
+    }
+    else if (input.includes("polymorphism")) {
+        response = "Polymorphism means one method can have many forms.";
+    }
+
+    chat.innerHTML += "<p><b>You:</b> " + input + "</p>";
+    chat.innerHTML += "<p><b>AI:</b> " + response + "</p>";
+
+    document.getElementById("userInput").value = "";
+}
+
+function toggleChat() {
+    let chat = document.getElementById("chatbot");
+    chat.classList.toggle("hidden");
+}
+
